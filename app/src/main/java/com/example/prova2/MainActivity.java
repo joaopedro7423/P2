0bbd2a3dbed218e4288653c2fef1b2460ef36752
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2020-11-23 19:58:53 -0400

    Add o firebase e implementando o cadastro

--- app/src/main/java/com/example/prova2/MainActivity.java
+++ app/src/main/java/com/example/prova2/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.prova2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.HashMap;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +35,30 @@ public class MainActivity extends AppCompatActivity {
         btCadastro = findViewById(R.id.btCadastro);
 
 
+
+        btCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("nome", editName.getText().toString());
+                map.put("idade", editTextNumber.getText().toString());
+
+                FirebaseDatabase.getInstance().getReference().child("Pessoas").push()
+                        .setValue(map)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.i("jooj","onComplete");
+                            }
+
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("yeyt","onFailure"+e.toString());
+                    }
+                });
+            };
+
+        });
     }
 }
