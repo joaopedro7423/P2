5ed1f4a0099f7e561260fdec47eb9e2d5567326a
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2020-11-23 19:32:03 -0400

    declarações de cadastro e tela

--- app/src/main/java/com/example/prova2/MainActivity.java
+++ app/src/main/java/com/example/prova2/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.prova2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText editName, editTextNumber;
+    private Button btCadastro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editName = findViewById(R.id.editName);
+        editTextNumber = findViewById(R.id.editTextNumber);
+
+        btCadastro = findViewById(R.id.btCadastro);
+
+
     }
 }
